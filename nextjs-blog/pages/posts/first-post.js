ff6fb2fbcb886764f776ad7771ec495a30347d87
Cássia Sanctos <csamp@ibm.com>
2023-11-08 11:49:22 -0300

    adding layout component

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import Head from "next/head";
-import Script from "next/script"; 
+// import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
     // return <h1>First Post</h1>;
     return(
-        <>
+        <Layout>
             <Head> 
                 <title>First Post</title>
             </Head>
@@ -20,6 +21,6 @@ export default function FirstPost() {
             <h2>
                 <Link href="/">← Back to home</Link>
             </h2>
-        </>
+        </Layout>
     );
   }
